46fc57e6810d94e81085633c7c298f6fbb6cb7fa
Andrés Durán C <aduran9@hotmail.com>
2021-10-14 16:38:40 -0500

    Carga de proyecto SpringBoot

---
+++ src/main/java/grupo25grupo0/minticgrupo25/Minticgrupo25Application.java
@@ -0,0 +1,13 @@
+package grupo25grupo0.minticgrupo25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Minticgrupo25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Minticgrupo25Application.class, args);
+	}
+
+}
---
+++ src/test/java/grupo25grupo0/minticgrupo25/Minticgrupo25ApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo25grupo0.minticgrupo25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Minticgrupo25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
